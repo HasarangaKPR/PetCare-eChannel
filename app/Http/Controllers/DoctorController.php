2eea063674d9f3c8f32822d00ea75b073a4ccbf0
Ravindu Hasaranga <hasarangakpr@gmail.com>
2024-09-01 23:23:30 +0530

    Create add doctor function

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    public function addDoctor(Request $request)
+    {
+        $validatedData = $request->validate([
+        'doctorName' => 'required|string|max:255',
+        'doctorDistrict' => 'required|string|max:255',
+        'doctorCity' => 'required|string|max:255',
+        'doctorContactNumber' => 'required|string|max:255|regex:/^\+?[0-9]{7,15}$/',
+        'doctorEmail' => 'required|string|email|max:255',
+        ]);
+
+
+        $channellingcenter = ChannellingCenter::create([
+            'doctorName' => $validatedData['doctorName'],
+            'doctorDistrict' => $validatedData['doctorDistrict'],
+            'doctorCity' => $validatedData['doctorCity'],
+            'doctorContactNumber' => $validatedData['doctorContactNumber'],
+            'doctorEmail' => $validatedData['doctorEmail'],
+            
+        ]);
+
+        //return redirect()->route('dashboard')->with('success', 'Added successfully.');
+        return response()->json(['success' => true]);
+
+    }
+        public function searchDoctor(Request $request)
+    {
+        $district = $request->input('doctorDistrict');
+        $city = $request->input('doctorCity');
+
+        $query = ChannellingCenter::query();
+
+        if ($district) {
+            $query->where('doctorDistrict', $district);
+        }
+        if ($city) {
+            $query->where('doctorCity', $city);
+        }
+        $doctors = $query->get();
+
+        return response()->json(['doctors' => $doctors]);
+    }
+}
---
+++ app/Models/Doctor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    //protected $table = 'channellingcenters';
+
+    protected $fillable = [
+        'doctorName',
+        'doctorDistrict',
+        'doctorCity',
+        'doctorContactNumber',
+        'doctorEmail',
+    ];
+}
