fac8908fc64474473719ce1d080403845166c72c
Ravindu Hasaranga <hasarangakpr@gmail.com>
2024-09-01 23:11:12 +0530

    Create add user function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function addUser(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'userType' => 'required|string',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'userType' => $validatedData['userType'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+            
+        ]);
+
+        //return redirect()->route('dashboard')->with('success', 'Added successfully.');
+        return response()->json(['success' => true]);
+
+    }
+    public function allUsers()
+    {
+        $users = User::all();
+        return response()->json(['users' => $users]);
+    }
+}
