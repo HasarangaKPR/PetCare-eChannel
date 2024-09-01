3698a305cbda20e0e0ddec853578cdb7ffdce88a
Ravindu Hasaranga <hasarangakpr@gmail.com>
2024-09-01 23:39:25 +0530

    Create routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Add or Display Users
+Route::post('/addUser', [UserController::class, 'addUser'])->name('addUser');
+Route::get('/allUsers', [UserController::class, 'allUsers'])->name('allUsers');
+
+//Add Chanelling Centers & Search Chanelling Centers
+Route::post('/addDoctor', [DoctorController::class, 'addDoctor'])->name('addDoctor');
+Route::get('/searchDoctor', [DoctorController::class, 'searchDoctor'])->name('searchDoctor');
+
+//Add Chanelling Centers Appointments
+Route::post('/addDoctorAppointment', [DoctorAppointmentController::class, 'addDoctorAppointment'])->name('addDoctorAppointment');
+Route::get('/displayToDoctorAppointment', [DoctorAppointmentController::class, 'displayToDoctorAppointment'])->name('displayToDoctorAppointment');
+Route::get('/displayToUserDoctorAppointment', [DoctorAppointmentController::class, 'displayToUserDoctorAppointment'])->name('displayToUserDoctorAppointment');
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,16 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//Add or Display Users
+Route::post('/addUser', [UserController::class, 'addUser'])->name('addUser');
+Route::get('/allUsers', [UserController::class, 'allUsers'])->name('allUsers');
+
+//Add Doctors & Search Doctors
+Route::post('/addDoctor', [DoctorController::class, 'addDoctor'])->name('addDoctor');
+Route::get('/searchDoctor', [DoctorController::class, 'searchDoctor'])->name('searchDoctor');
+
+//Add Doctors Appointments
+Route::post('/addDoctorAppointment', [DoctorAppointmentController::class, 'addDoctorAppointment'])->name('addDoctorAppointment');
+Route::get('/displayToDoctorAppointment', [DoctorAppointmentController::class, 'displayToDoctorAppointment'])->name('displayToDoctorAppointment');
+Route::get('/displayToUserDoctorAppointment', [DoctorAppointmentController::class, 'displayToUserDoctorAppointment'])->name('displayToUserDoctorAppointment');
