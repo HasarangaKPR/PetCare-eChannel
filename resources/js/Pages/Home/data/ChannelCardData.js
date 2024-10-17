52f058588b86947297b3f08a57cd3bab7ba0e6fe
Kavindi98-lab <kavindinirashani98@gmail.com>
2024-10-17 19:07:53 +0530

    add raw data

---
+++ resources/js/Pages/Home/data/ChannelCardData.js
@@ -0,0 +1,40 @@
+import Avatar1 from "../assets/HomePage/Avatar1.svg";
+import Avatar2 from "../assets/HomePage/Avatar2.svg";
+import Avatar3 from "../assets/HomePage/Avatar3.svg";
+import Avatar4 from "../assets/HomePage/Avatar4.svg";
+
+export const ChannelCardData = [
+    {
+      imageSrc: Avatar1,
+      title: "DOCTOR CHANNELING",
+      description: "Channel a Doctor",
+      bgColor: "bg-[#156862]",
+      textColor: "text-white",
+      textSecondaryColor: "text-[#B4C0CC]",
+    },
+    {
+      imageSrc: Avatar2,
+      title: "DAYCARE BOOKINGS",
+      description: "Book a Daycare",
+      bgColor: "bg-white",
+      textColor: "text-[#08101A]",
+      textSecondaryColor: "text-[#737B83]",
+    },
+    {
+      imageSrc: Avatar4,
+      title: "PETCARE SERVICES",
+      description: "Pet Services",
+      bgColor: "bg-white",
+      textColor: "text-[#08101A]",
+      textSecondaryColor: "text-[#737B83]",
+    },
+    {
+      imageSrc: Avatar3,
+      title: "PET BUYING & SELLING",
+      description: "Buy a Pet",
+      bgColor: "bg-white",
+      textColor: "text-[#08101A]",
+      textSecondaryColor: "text-[#737B83]",
+    },
+  ];
+  
