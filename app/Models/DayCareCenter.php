669e9aa892cd702b379cc6b141eac71a0f43e515
Ravindu Hasaranga <kprhbis@gmail.com>
2024-10-18 20:26:29 +0530

    Create DayCareCenter add function

---
+++ app/Models/DayCareCenter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DayCareCenter extends Model
+{
+    use HasFactory;
+
+    //protected $table = 'daycarecenters';
+
+    protected $fillable = [
+        'dayCareCenterName',
+        'dayCareCenterDistrict',
+        'dayCareCenterCity',
+        'dayCareCenterContactNumber',
+        'dayCareCenterEmail',
+        'noOfRooms',
+    ];
+}
