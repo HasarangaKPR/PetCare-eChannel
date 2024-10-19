6518836b40f76b9edfc60cac03e2ff13556bf77c
maduranga00 <maduranga0713@gmail.com>
2024-10-19 22:38:59 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PetAdController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +26,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+//add pet ad
+Route::post('/createAd',[PetAdController::class, 'createAd'])->name('createAd');
