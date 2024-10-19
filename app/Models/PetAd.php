7b6fe765a75b546f1695a667925c655d1e1e0cc2
maduranga00 <maduranga0713@gmail.com>
2024-10-19 22:38:49 +0530

    Update PetAd.php

--- app/Models/PetAd.php
+++ app/Models/PetAd.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class PetAd extends Model
 {
     use HasFactory;
+    protected $table = 'pet_ads';
+    protected $fillable = [
+        'name',
+        'address',
+        'breed',
+        'description',
+        'contactnumber',
+        'price',
+
+
+
+    ];
 }
