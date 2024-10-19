2d14d700b165578a900b965c0509c0442f1d0702
maduranga00 <maduranga0713@gmail.com>
2024-10-19 22:38:44 +0530

    Update PetAdController.php

--- app/Http/Controllers/PetAdController.php
+++ app/Http/Controllers/PetAdController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PetAd;
 use Illuminate\Http\Request;
 
 class PetAdController extends Controller
 {
-    //
+    public function createAd(Request $request)
+    {
+    $validated = $request->validate([
+        'name' => 'required|string|max:255',
+        'address' => 'required|string|max:255',
+        'breed' => 'required',
+        'description' => 'required|string|max:255',
+        'contactnumber'=>'required|string|max:255|regex:/^\+?[0-9]{7,15}$/',
+        'price' => 'required|decimal',
+        //'image' => 'image|max:2048', // Validate image
+    ]);
+
+    $pets=PetAd::create([
+        'name' => $validated['name'],
+        'address' => $validated['address'],
+        'breed' => $validated['breed'],
+        'description' => $validated['description'],
+        'contactnumber'=>$validated['contactnumber'],
+        'price' => $validated['price'],
+
+    ]);
+    return response()->json(['success'=>true]);    
+    }
 }
