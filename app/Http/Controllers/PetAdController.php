78cc9f446c2220fbafb7dd6ef378bf78b2827a16
maduranga00 <maduranga0713@gmail.com>
2024-10-20 14:33:34 +0530

    add some functions

--- app/Http/Controllers/PetAdController.php
+++ app/Http/Controllers/PetAdController.php
@@ -19,7 +19,7 @@ class PetAdController extends Controller
         //'image' => 'image|max:2048', // Validate image
     ]);
 
-    $pets=PetAd::create([
+    PetAd::create([
         'name' => $validated['name'],
         'address' => $validated['address'],
         'breed' => $validated['breed'],
@@ -29,5 +29,42 @@ class PetAdController extends Controller
 
     ]);
     return response()->json(['success'=>true,'message' => 'Ad posted successfully']);    
+    }
+    public function updateAd(Request $request){
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'breed' => 'required',
+            'description' => 'required|string|max:255',
+            'contactnumber'=>'required|string|max:255|regex:/^\+?[0-9]{7,15}$/',
+            'price' => 'required|decimal:2',
+            //'image' => 'image|max:2048', // Validate image
+        ]);
+    
+        PetAd::updateAd([
+            'name' => $validated['name'],
+            'address' => $validated['address'],
+            'breed' => $validated['breed'],
+            'description' => $validated['description'],
+            'contactnumber'=>$validated['contactnumber'],
+            'price' => $validated['price'],
+    
+        ]);
+        return response()->json(['success'=>true,'message' => 'Ad updated successfully']);
+
+    }
+    public function ediAd(Request $request){
+
+    }
+    public function showAd(Request $request){
+
+    }
+    public function destroyAd(PetAd $petAd){
+        $petAd->delete();
+        return response()->json(['success'=>true,'message' => 'Ad delete successfully']);
+    }
+    public function index(){
+         // Retrieve all th ads from the database
+         PetAd::all();
     }
 }
