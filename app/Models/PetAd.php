4a102020d9f3540705c2a03b0905571bd505a483
maduranga00 <maduranga0713@gmail.com>
2024-10-26 20:03:06 +0530

    insert new data

--- app/Models/PetAd.php
+++ app/Models/PetAd.php
@@ -8,17 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class PetAd extends Model
 {
     use HasFactory;
+    // Define the table name (optional if the table name matches the pluralized model name)
     protected $table = 'pet_ads';
+    // Define the fillable fields to allow mass assignment
     protected $fillable = [
-        //'adId',
-        'name',
-        'address',
+        'pet_name',
+        'pet_type',
         'breed',
-        'description',
-        'contactnumber',
+        'age',
+        'gender',
         'price',
-
-
-
+        'description',
+       // 'pet_photos',
+        'seller_name',
+        'phone_number',
+        'location',
     ];
+     // Cast 'pet_photos' as an array
+     //protected $casts = [
+       // 'pet_photos' => 'array',
+    // ];
 }
