07b228768848f4906b159e176d1aee845aa50360
maduranga00 <maduranga0713@gmail.com>
2024-11-04 08:31:18 +0530

    Update DayCareCenterConfirmationMail.blade.php

--- resources/views/email/DayCareCenterConfirmationMail.blade.php
+++ resources/views/email/DayCareCenterConfirmationMail.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DayCare Center Registration Confirmation!</title>
+    <title>Daycare Center Registration Confirmation</title>
 </head>
 <body>
     <h1>Welcome, {{ $user->name }}!</h1>
-    <p>Thank you for registering on our platform. We're excited to have you on board.</p>
-    <p>If you have any questions, feel free to reach out to us.</p>
-    <p>Best regards,<br>Team</p>
+    <p>Your registration as a daycare center has been confirmed. You can now log in to your account and access the platform.</p>
+    <p>Thank you for joining us!</p>
 </body>
 </html>
