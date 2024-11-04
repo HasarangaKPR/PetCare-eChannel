ddebca26e18fbdc464d347f77ff2ca343a4ce37d
VihangaRandinu99 <120121708+VihangaRandinu99@users.noreply.github.com>
2024-11-04 17:51:01 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdController;
 
-
-
-
-//pets ads
-
-Route::get('/ads/create', [AdController::class, 'create'])->name('ads.create');
-Route::post('/ads', [AdController::class, 'store'])->name('ads.store');
-Route::get('/ads', [AdController::class, 'index'])->name('ads.index'); // To list all ads
-Route::resource('ads', AdController::class); // post ad 
-Route::get('/ads', [AdController::class, 'index'])->name('ads.index'); // Search button
 
 
 
